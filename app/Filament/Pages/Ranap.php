aa80eda7ab92b9d35268749158e8d0baffa59b86
Yudo <53389941+juniz@users.noreply.github.com>
2024-12-19 18:29:21 +0700

    ubah letak tombol hais di depan

--- app/Filament/Pages/Ranap.php
+++ app/Filament/Pages/Ranap.php
@@ -23,6 +23,7 @@ use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Notifications\Notification;
+use Filament\Tables\Enums\ActionsPosition;
 use Filament\Actions;
 use Filament\Forms\Form;
 
@@ -88,176 +89,174 @@ class Ranap extends Page implements HasTable
                     ->sortable(),
             ])
             ->actions([
-                Tables\Actions\ActionGroup::make([
-                    Tables\Actions\CreateAction::make('hais')
-                        ->label('Data HAIs')
-                        ->modalHeading('Data HAIs')
-                        ->mountUsing(function (Form $form, RegPeriksa $regPeriksa) {
-                            $data = \App\Models\DataHais::where('no_rawat', $regPeriksa->no_rawat)->first();
-                            if ($data) {
-                                $form->fill($data->toArray());
-                            } else {
-                                $form->fill([
-                                    'tanggal' => now(),
-                                    'DEKU' => 'TIDAK',
-                                    'SPUTUM' => '',
-                                    'DARAH' => '',
-                                    'URINE' => '',
-                                    'ETT' => 0,
-                                    'CVL' => 0,
-                                    'IVL' => 0,
-                                    'UC' => 0,
-                                    'VAP' => 0,
-                                    'IAD' => 0,
-                                    'PLEB' => 0,
-                                    'ISK' => 0,
-                                    'ILO' => 0,
-                                    'HAP' => 0,
-                                    'Tinea' => 0,
-                                    'Scabies' => 0,
-                                    'ANTIBIOTIK' => '',
-                                ]);
-                            }
-                        })
-                        ->action(function (array $data, RegPeriksa $regPeriksa) {
-                            try {
-                                $data['no_rawat'] = $regPeriksa->no_rawat;
-                                $kamar = \App\Models\KamarInap::where('no_rawat', $regPeriksa->no_rawat)->where('tgl_keluar', '0000-00-00')->first();
-                                $data['kd_kamar'] = $kamar->kd_kamar;
-                                \App\Models\DataHais::updateOrCreate([
-                                    'no_rawat' => $regPeriksa->no_rawat,
-                                    'tanggal' => $data['tanggal'],
-                                ], $data);
+                Tables\Actions\CreateAction::make('hais')
+                    ->label('Data HAIs')
+                    ->modalHeading('Data HAIs')
+                    ->mountUsing(function (Form $form, RegPeriksa $regPeriksa) {
+                        $data = \App\Models\DataHais::where('no_rawat', $regPeriksa->no_rawat)->first();
+                        if ($data) {
+                            $form->fill($data->toArray());
+                        } else {
+                            $form->fill([
+                                'tanggal' => now(),
+                                'DEKU' => 'TIDAK',
+                                'SPUTUM' => '',
+                                'DARAH' => '',
+                                'URINE' => '',
+                                'ETT' => 0,
+                                'CVL' => 0,
+                                'IVL' => 0,
+                                'UC' => 0,
+                                'VAP' => 0,
+                                'IAD' => 0,
+                                'PLEB' => 0,
+                                'ISK' => 0,
+                                'ILO' => 0,
+                                'HAP' => 0,
+                                'Tinea' => 0,
+                                'Scabies' => 0,
+                                'ANTIBIOTIK' => '',
+                            ]);
+                        }
+                    })
+                    ->action(function (array $data, RegPeriksa $regPeriksa) {
+                        try {
+                            $data['no_rawat'] = $regPeriksa->no_rawat;
+                            $kamar = \App\Models\KamarInap::where('no_rawat', $regPeriksa->no_rawat)->where('tgl_keluar', '0000-00-00')->first();
+                            $data['kd_kamar'] = $kamar->kd_kamar;
+                            \App\Models\DataHais::updateOrCreate([
+                                'no_rawat' => $regPeriksa->no_rawat,
+                                'tanggal' => $data['tanggal'],
+                            ], $data);
 
-                                Notification::make()
-                                    ->title('Data HAIs berhasil disimpan')
-                                    ->success()
-                                    ->send();
+                            Notification::make()
+                                ->title('Data HAIs berhasil disimpan')
+                                ->success()
+                                ->send();
 
-                                $this->redirect('/data-hais?tableSearch=' . $regPeriksa->no_rawat);
-                            } catch (\Exception $e) {
-                                // dd($e->getMessage());
-                                Notification::make()
-                                    ->title('Data Pasien Gagal Disimpan')
-                                    ->body($e->getMessage())
-                                    ->danger()
-                                    ->send();
-                            }
-                        })
-                        ->form([
-                            Split::make([
-                                Section::make([
-                                    DatePicker::make('tanggal')
-                                        ->label('Tanggal')
-                                        ->default(now())
-                                        ->required(),
-                                    Select::make('DEKU')
-                                        ->label('Deku')
-                                        ->options([
-                                            'IYA' => 'IYA',
-                                            'TIDAK' => 'TIDAK'
-                                        ])
-                                        ->default('Tidak')
-                                        ->required(),
-                                    TextInput::make('SPUTUM')
-                                        ->label('Sputum')
-                                        ->default('')
-                                        ->required(),
-                                    TextInput::make('DARAH')
-                                        ->label('Darah')
-                                        ->default('')
-                                        ->required(),
-                                    TextInput::make('URINE')
-                                        ->label('Urine')
-                                        ->default('')
-                                        ->required(),
-                                ]),
-                                Section::make([
-                                    Section::make('Hari Pemasangan Alat')
-                                        ->schema([
-                                            TextInput::make('ETT')
-                                                ->label('ETT')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('CVL')
-                                                ->label('CVL')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('IVL')
-                                                ->label('IVL')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('UC')
-                                                ->label('UC')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                        ])
-                                        ->columns(4),
-                                    Section::make('Infeksi RS')
-                                        ->schema([
-                                            TextInput::make('VAP')
-                                                ->label('VAP')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('IAD')
-                                                ->label('IAD')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('PLEB')
-                                                ->label('PLEB')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('ISK')
-                                                ->label('ISK')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('ILO')
-                                                ->label('ILO')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('HAP')
-                                                ->label('HAP')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('Tinea')
-                                                ->label('Tinea')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                            TextInput::make('Scabies')
-                                                ->label('Scabies')
-                                                ->numeric()
-                                                ->default('0')
-                                                ->required(),
-                                        ])
-                                        ->columns(4),
-                                    TextInput::make('ANTIBIOTIK')
-                                        ->label('Antibiotik')
-                                        ->default('')
-                                        ->required(),
-                                    // TextInput::make('TIRAH')
-                                    //     ->label('Tirah Baring')
-                                    //     ->default('1')
-                                    //     ->required(),
-                                ]),
-                            ])
-                                ->from('md')
+                            $this->redirect('/data-hais?tableSearch=' . $regPeriksa->no_rawat);
+                        } catch (\Exception $e) {
+                            // dd($e->getMessage());
+                            Notification::make()
+                                ->title('Data Pasien Gagal Disimpan')
+                                ->body($e->getMessage())
+                                ->danger()
+                                ->send();
+                        }
+                    })
+                    ->form([
+                        Split::make([
+                            Section::make([
+                                DatePicker::make('tanggal')
+                                    ->label('Tanggal')
+                                    ->default(now())
+                                    ->required(),
+                                Select::make('DEKU')
+                                    ->label('Deku')
+                                    ->options([
+                                        'IYA' => 'IYA',
+                                        'TIDAK' => 'TIDAK'
+                                    ])
+                                    ->default('Tidak')
+                                    ->required(),
+                                TextInput::make('SPUTUM')
+                                    ->label('Sputum')
+                                    ->default('')
+                                    ->required(),
+                                TextInput::make('DARAH')
+                                    ->label('Darah')
+                                    ->default('')
+                                    ->required(),
+                                TextInput::make('URINE')
+                                    ->label('Urine')
+                                    ->default('')
+                                    ->required(),
+                            ]),
+                            Section::make([
+                                Section::make('Hari Pemasangan Alat')
+                                    ->schema([
+                                        TextInput::make('ETT')
+                                            ->label('ETT')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('CVL')
+                                            ->label('CVL')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('IVL')
+                                            ->label('IVL')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('UC')
+                                            ->label('UC')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                    ])
+                                    ->columns(4),
+                                Section::make('Infeksi RS')
+                                    ->schema([
+                                        TextInput::make('VAP')
+                                            ->label('VAP')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('IAD')
+                                            ->label('IAD')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('PLEB')
+                                            ->label('PLEB')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('ISK')
+                                            ->label('ISK')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('ILO')
+                                            ->label('ILO')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('HAP')
+                                            ->label('HAP')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('Tinea')
+                                            ->label('Tinea')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                        TextInput::make('Scabies')
+                                            ->label('Scabies')
+                                            ->numeric()
+                                            ->default('0')
+                                            ->required(),
+                                    ])
+                                    ->columns(4),
+                                TextInput::make('ANTIBIOTIK')
+                                    ->label('Antibiotik')
+                                    ->default('')
+                                    ->required(),
+                                // TextInput::make('TIRAH')
+                                //     ->label('Tirah Baring')
+                                //     ->default('1')
+                                //     ->required(),
+                            ]),
                         ])
-                        ->modalWidth(MaxWidth::Full)
-                        // ->modalSubmitAction(false)
-                        ->modalCancelActionLabel('Batal')
-                        ->modalSubmitActionLabel('Simpan'),
-                ])
-            ]);
+                            ->from('md')
+                    ])
+                    ->modalWidth(MaxWidth::Full)
+                    // ->modalSubmitAction(false)
+                    ->modalCancelActionLabel('Batal')
+                    ->modalSubmitActionLabel('Simpan'),
+            ], position: ActionsPosition::BeforeColumns);
     }
 }
